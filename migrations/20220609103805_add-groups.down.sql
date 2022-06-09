daf4f0168d5a10f3f5efaf4d89aed3aa339a1e11
Orhun Parmaksiz <orhun@blockjoy.com>
2022-06-09 14:01:15 +0300

    Add database migrations for groups

---
+++ migrations/20220609103805_add-groups.down.sql
@@ -0,0 +1,3 @@
+DROP TABLE IF EXISTS groupable;
+DROP TABLE IF EXISTS groups;
+DROP TYPE IF EXISTS enum_groupable_type;
---
+++ migrations/20220609103805_add-groups.up.sql
@@ -0,0 +1,17 @@
+CREATE TYPE enum_groupable_type AS ENUM ('hosts', 'nodes');
+
+CREATE TABLE IF NOT EXISTS groups (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    name Text NOT NULL,
+    org_id UUID NOT NULL REFERENCES orgs(id) ON DELETE CASCADE,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT now()
+);
+CREATE UNIQUE INDEX IF NOT EXISTS idx_groups_org_id on groups(org_id);
+
+CREATE TABLE IF NOT EXISTS groupable (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    group_id UUID NOT NULL REFERENCES groups(id) ON DELETE CASCADE,
+    groupable_id UUID NOT NULL,
+    groupable_type enum_groupable_type NOT NULL
+);
