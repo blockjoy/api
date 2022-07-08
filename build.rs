70617b826c6bee1d1d07acff7ab36b88a0506d87
tstaetter <thomas.staetter@gmail.com>
2022-07-08 16:43:04 +0200

    feat: added protos for nodes, hosts, commands

--- build.rs
+++ build.rs
@@ -3,7 +3,15 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
         .build_server(true)
-        .compile(&["proto/api/api_services.proto"], &["proto", "proto/api"])?;
+        .compile(
+            &[
+                "proto/api/api_services.proto",
+                "proto/api/node_service.proto",
+                "proto/api/host_service.proto",
+                "proto/api/command_service.proto",
+            ],
+            &["proto", "proto/api"]
+        )?;
 
     Ok(())
 }
