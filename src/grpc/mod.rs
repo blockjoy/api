359bc9046ed43c89eb6474bcb19bc492aefbb2f7
tstaetter <thomas.staetter@gmail.com>
2022-07-08 16:43:22 +0200

    refactor: removed obsolete fn

--- src/grpc/mod.rs
+++ src/grpc/mod.rs
@@ -1,5 +1,15 @@
-use tonic::{metadata::MetadataValue, Request, Status};
+use tonic::{Request, Status};
 
 pub mod blockjoy {
     tonic::include_proto!("blockjoy.api");
+}
+
+/// Authenticate user identified by Bearer token
+///
+/// # Params
+/// ***req:*** The incoming request. If successful, the user will be added to the request as
+///     extension
+#[allow(dead_code)]
+fn authenticate_bearer(mut _req: Request<()>) -> Result<Request<()>, Status> {
+    Ok(_req)
 }
