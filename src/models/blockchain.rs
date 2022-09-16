4825342475c2775057d93172b91dad6d8bbc5f37
Luuk Wester <luuk@blockjoy.com>
2022-09-16 17:42:10 +0200

    add supported_node_types to blockchains

--- src/models/blockchain.rs
+++ src/models/blockchain.rs
@@ -1,4 +1,7 @@
-use crate::errors::{ApiError, Result};
+use crate::{
+    errors::{ApiError, Result},
+    grpc::blockjoy_ui,
+};
 use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
 use sqlx::{FromRow, PgPool};
@@ -35,11 +38,41 @@ pub struct Blockchain {
 
 impl Blockchain {
     pub async fn find_all(db: &PgPool) -> Result<Vec<Self>> {
-        sqlx::query_as::<_, Self>(
-            "SELECT * FROM blockchains WHERE status <> 'deleted' order by lower(name)",
-        )
-        .fetch_all(db)
-        .await
-        .map_err(ApiError::from)
+        sqlx::query_as("SELECT * FROM blockchains WHERE status <> 'deleted' order by lower(name)")
+            .fetch_all(db)
+            .await
+            .map_err(ApiError::from)
+    }
+
+    pub async fn find_by_id(id: uuid::Uuid, db: &PgPool) -> Result<Self> {
+        sqlx::query_as("SELECT * FROM blockchains WHERE status <> 'deleted' AND id = $1")
+            .bind(id)
+            .fetch_one(db)
+            .await
+            .map_err(ApiError::from)
+    }
+}
+
+impl From<Blockchain> for blockjoy_ui::Blockchain {
+    fn from(model: Blockchain) -> Self {
+        let convert_dt = |dt: DateTime<Utc>| prost_types::Timestamp {
+            seconds: dt.timestamp(),
+            nanos: dt.timestamp_nanos() as i32,
+        };
+        Self {
+            id: Some(model.id.into()),
+            name: model.name,
+            description: model.description,
+            status: model.status as i32,
+            project_url: model.project_url,
+            repo_url: model.repo_url,
+            supports_etl: model.supports_etl,
+            supports_node: model.supports_node,
+            supports_staking: model.supports_staking,
+            supports_broadcast: model.supports_broadcast,
+            version: model.version,
+            created_at: Some(convert_dt(model.created_at)),
+            updated_at: Some(convert_dt(model.updated_at)),
+        }
     }
 }
